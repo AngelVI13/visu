86826b0536766acf79c204f0ac74caf9b224b8d2
Angel ILIEV <a.v.iliev13@gmail.com>
2020-10-24 14:23:26 +0300

    Added logic to find non overlapping squares

--- src/models/board.py
+++ src/models/board.py
@@ -1,8 +1,10 @@
 import time
 from abc import ABC, abstractmethod
 from typing import List
-from functools import partial
+from operator import xor
+from functools import partial, reduce
 from itertools import cycle, chain
+from models.square import Square
 
 
 class Board:
@@ -13,5 +15,12 @@ class Board:
         self.pieces = []
 
     @property
-    def occupied(self):
+    def occupied(self) -> List[Square]:
+        """Get list of occupied squares."""
         return [piece.square for piece in self.pieces]
+
+    def get_singular_squares(self) -> List[Square]:
+        """Return the list of squares to which only 1 piece can go."""
+        all_moves = [set(piece.get_moves()) for piece in self.pieces]
+        # set(a) ^ set(b) = non-intersecting elements from a and b
+        return list(reduce(xor, all_moves))
---
+++ tests/test_board.py
@@ -0,0 +1,29 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join("..", "src")))
+
+from pytest import raises
+from models.board import Board
+from models.square import Square
+from models.pieces import Bishop, Knight
+
+
+def test_get_singular_squares():
+    board = Board()
+
+    bishop = Bishop(Square.from_notation("a1"), board)
+    knight = Knight(Square.from_notation("b1"), board)
+
+    board.pieces.append(bishop)
+    board.pieces.append(knight)
+
+    squares = board.get_singular_squares()
+    assert isinstance(squares, list)
+    assert all(isinstance(square, Square) for square in squares)
+    assert len(squares) == 8
+    assert Square.from_notation("b2") in squares
+    assert Square.from_notation("h8") in squares
+    assert Square.from_notation("c3") not in squares
+    assert Square.from_notation("a3") in squares
+    assert Square.from_notation("d2") in squares
+
