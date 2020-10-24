cea0ce0873e299053a4dd03d0e75a7d78837a7c4
Angel ILIEV <a.v.iliev13@gmail.com>
2020-10-24 14:04:57 +0300

    Converted tests to use notation instead of indexes

--- tests/test_pieces.py
+++ tests/test_pieces.py
@@ -17,8 +17,8 @@ def test_bishop_moves():
     moves = bishop.get_moves()
     assert len(moves) == 7
     assert all(isinstance(move, Square) for move in moves)
-    assert Square(file=1, rank=1) in moves
-    assert Square(file=7, rank=7) in moves
+    assert Square.from_notation("b2") in moves
+    assert Square.from_notation("h8") in moves
 
     # Check move generation when bishop is on D4
     bishop = Bishop(Square.from_notation("d4"), board)
@@ -26,10 +26,10 @@ def test_bishop_moves():
     moves = bishop.get_moves()
     assert len(moves) == 13
     assert all(isinstance(move, Square) for move in moves)
-    assert Square(file=1, rank=1) in moves
-    assert Square(file=7, rank=7) in moves
-    assert Square(file=0, rank=6) in moves
-    assert Square(file=6, rank=0) in moves
+    assert Square.from_notation("b2") in moves
+    assert Square.from_notation("h8") in moves
+    assert Square.from_notation("a7") in moves
+    assert Square.from_notation("g1") in moves
 
     # Check move generation when bishop is on E4
     # and there is a blockin piece on F5
@@ -41,10 +41,10 @@ def test_bishop_moves():
     moves = bishop.get_moves()
     assert len(moves) == 10
     assert all(isinstance(move, Square) for move in moves)
-    assert Square(file=1, rank=0) in moves
-    assert Square(file=7, rank=0) in moves
-    assert Square(file=0, rank=7) in moves
-    assert Square(file=7, rank=6) not in moves
+    assert Square.from_notation("b1") in moves
+    assert Square.from_notation("h1") in moves
+    assert Square.from_notation("a8") in moves
+    assert Square.from_notation("h7") not in moves
 
 def test_rook_moves():
     board = Board()
@@ -55,8 +55,8 @@ def test_rook_moves():
     moves = rook.get_moves()
     assert len(moves) == 14
     assert all(isinstance(move, Square) for move in moves)
-    assert Square(file=7, rank=0) in moves
-    assert Square(file=0, rank=7) in moves
+    assert Square.from_notation("h1") in moves
+    assert Square.from_notation("a8") in moves
 
     # Check move generation when rook is on D4
     rook = Rook(Square.from_notation("d4"), board)
@@ -64,10 +64,10 @@ def test_rook_moves():
     moves = rook.get_moves()
     assert len(moves) == 14
     assert all(isinstance(move, Square) for move in moves)
-    assert Square(file=0, rank=3) in moves
-    assert Square(file=7, rank=3) in moves
-    assert Square(file=3, rank=7) in moves
-    assert Square(file=3, rank=0) in moves
+    assert Square.from_notation("a4") in moves
+    assert Square.from_notation("h4") in moves
+    assert Square.from_notation("d8") in moves
+    assert Square.from_notation("d1") in moves
 
     # Check move generation when rook is on D4
     # and there is a blockin piece on F4
@@ -79,11 +79,11 @@ def test_rook_moves():
     moves = rook.get_moves()
     assert len(moves) == 11
     assert all(isinstance(move, Square) for move in moves)
-    assert Square(file=0, rank=3) in moves
-    assert Square(file=4, rank=3) in moves
-    assert Square(file=3, rank=0) in moves
-    assert Square(file=3, rank=7) in moves
-    assert Square(file=5, rank=3) not in moves
+    assert Square.from_notation("a4") in moves
+    assert Square.from_notation("e4") in moves
+    assert Square.from_notation("d1") in moves
+    assert Square.from_notation("d8") in moves
+    assert Square.from_notation("f4") not in moves
 
 def test_queen_moves():
     board = Board()
@@ -94,9 +94,9 @@ def test_queen_moves():
     moves = queen.get_moves()
     assert len(moves) == 21
     assert all(isinstance(move, Square) for move in moves)
-    assert Square(file=7, rank=0) in moves
-    assert Square(file=0, rank=7) in moves
-    assert Square(file=7, rank=7) in moves
+    assert Square.from_notation("h1") in moves
+    assert Square.from_notation("a8") in moves
+    assert Square.from_notation("h8") in moves
 
     # Check move generation when queen is on D4
     queen = Queen(Square.from_notation("d4"), board)
@@ -104,14 +104,14 @@ def test_queen_moves():
     moves = queen.get_moves()
     assert len(moves) == 27
     assert all(isinstance(move, Square) for move in moves)
-    assert Square(file=0, rank=3) in moves
-    assert Square(file=7, rank=3) in moves
-    assert Square(file=3, rank=7) in moves
-    assert Square(file=3, rank=0) in moves
-    assert Square(file=1, rank=1) in moves
-    assert Square(file=7, rank=7) in moves
-    assert Square(file=0, rank=6) in moves
-    assert Square(file=6, rank=0) in moves
+    assert Square.from_notation("a4") in moves
+    assert Square.from_notation("h4") in moves
+    assert Square.from_notation("d8") in moves
+    assert Square.from_notation("d1") in moves
+    assert Square.from_notation("b2") in moves
+    assert Square.from_notation("h8") in moves
+    assert Square.from_notation("a7") in moves
+    assert Square.from_notation("g1") in moves
 
     # Check move generation when queen is on D4
     # and there is a blockin piece on F4
@@ -125,16 +125,16 @@ def test_queen_moves():
     moves = queen.get_moves()
     assert len(moves) == 21
     assert all(isinstance(move, Square) for move in moves)
-    assert Square(file=7, rank=3) in moves
-    assert Square(file=4, rank=3) in moves
-    assert Square(file=3, rank=0) in moves
-    assert Square(file=3, rank=7) in moves
-    assert Square(file=5, rank=3) in moves
-    assert Square(file=1, rank=5) in moves
-    assert Square(file=6, rank=0) in moves
-    assert Square(file=0, rank=6) in moves
-    assert Square(file=0, rank=0) not in moves
-    assert Square(file=0, rank=3) not in moves
+    assert Square.from_notation("h4") in moves
+    assert Square.from_notation("e4") in moves
+    assert Square.from_notation("d1") in moves
+    assert Square.from_notation("d8") in moves
+    assert Square.from_notation("f4") in moves
+    assert Square.from_notation("b6") in moves
+    assert Square.from_notation("g1") in moves
+    assert Square.from_notation("a7") in moves
+    assert Square.from_notation("a1") not in moves
+    assert Square.from_notation("a4") not in moves
 
 
 def test_knight_moves():
@@ -146,8 +146,8 @@ def test_knight_moves():
     moves = knight.get_moves()
     assert len(moves) == 2
     assert all(isinstance(move, Square) for move in moves)
-    assert Square(file=1, rank=2) in moves
-    assert Square(file=2, rank=1) in moves
+    assert Square.from_notation("b3") in moves
+    assert Square.from_notation("c2") in moves
 
     # Check move generation when knight is on D4
     knight = Knight(Square.from_notation("d4"), board)
@@ -155,14 +155,14 @@ def test_knight_moves():
     moves = knight.get_moves()
     assert len(moves) == 8
     assert all(isinstance(move, Square) for move in moves)
-    assert Square(file=1, rank=2) in moves
-    assert Square(file=1, rank=4) in moves
-    assert Square(file=2, rank=5) in moves
-    assert Square(file=2, rank=1) in moves
-    assert Square(file=4, rank=1) in moves
-    assert Square(file=4, rank=5) in moves
-    assert Square(file=5, rank=4) in moves
-    assert Square(file=5, rank=2) in moves
+    assert Square.from_notation("b3") in moves
+    assert Square.from_notation("b5") in moves
+    assert Square.from_notation("c6") in moves
+    assert Square.from_notation("c2") in moves
+    assert Square.from_notation("e2") in moves
+    assert Square.from_notation("e6") in moves
+    assert Square.from_notation("f5") in moves
+    assert Square.from_notation("f3") in moves
 
     # Check move generation when knight is on D4
     # and there is a blockin piece on C2
@@ -174,14 +174,14 @@ def test_knight_moves():
     moves = knight.get_moves()
     assert len(moves) == 7
     assert all(isinstance(move, Square) for move in moves)
-    assert Square(file=1, rank=2) in moves
-    assert Square(file=1, rank=4) in moves
-    assert Square(file=2, rank=5) in moves
-    assert Square(file=2, rank=1) not in moves
-    assert Square(file=4, rank=1) in moves
-    assert Square(file=4, rank=5) in moves
-    assert Square(file=5, rank=4) in moves
-    assert Square(file=5, rank=2) in moves
+    assert Square.from_notation("b3") in moves
+    assert Square.from_notation("b5") in moves
+    assert Square.from_notation("c6") in moves
+    assert Square.from_notation("c2") not in moves
+    assert Square.from_notation("e2") in moves
+    assert Square.from_notation("e6") in moves
+    assert Square.from_notation("f5") in moves
+    assert Square.from_notation("f3") in moves
 
 
 def test_king_moves():
@@ -194,8 +194,8 @@ def test_king_moves():
     assert len(moves) == 3
     assert all(isinstance(move, Square) for move in moves)
     assert Square(file=0, rank=1) in moves
-    assert Square(file=1, rank=1) in moves
-    assert Square(file=1, rank=0) in moves
+    assert Square.from_notation("b1") in moves
+    assert Square.from_notation("b1") in moves
 
     # Check move generation when king is on D4
     king = King(Square.from_notation("d4"), board)
@@ -203,14 +203,14 @@ def test_king_moves():
     moves = king.get_moves()
     assert len(moves) == 8
     assert all(isinstance(move, Square) for move in moves)
-    assert Square(file=2, rank=2) in moves
-    assert Square(file=2, rank=3) in moves
-    assert Square(file=2, rank=4) in moves
-    assert Square(file=3, rank=2) in moves
-    assert Square(file=3, rank=4) in moves
-    assert Square(file=4, rank=2) in moves
-    assert Square(file=4, rank=3) in moves
-    assert Square(file=4, rank=4) in moves
+    assert Square.from_notation("c3") in moves
+    assert Square.from_notation("c4") in moves
+    assert Square.from_notation("c5") in moves
+    assert Square.from_notation("d3") in moves
+    assert Square.from_notation("d5") in moves
+    assert Square.from_notation("e3") in moves
+    assert Square.from_notation("e4") in moves
+    assert Square.from_notation("e5") in moves
 
     # Check move generation when king is on D4
     # and there is a blockin piece on C5
@@ -222,11 +222,11 @@ def test_king_moves():
     moves = king.get_moves()
     assert len(moves) == 7
     assert all(isinstance(move, Square) for move in moves)
-    assert Square(file=2, rank=2) in moves
-    assert Square(file=2, rank=3) in moves
-    assert Square(file=2, rank=4) not in moves
-    assert Square(file=3, rank=2) in moves
-    assert Square(file=3, rank=4) in moves
-    assert Square(file=4, rank=2) in moves
-    assert Square(file=4, rank=3) in moves
-    assert Square(file=4, rank=4) in moves
+    assert Square.from_notation("c3") in moves
+    assert Square.from_notation("c4") in moves
+    assert Square.from_notation("c5") not in moves
+    assert Square.from_notation("d3") in moves
+    assert Square.from_notation("d5") in moves
+    assert Square.from_notation("e3") in moves
+    assert Square.from_notation("e4") in moves
+    assert Square.from_notation("e5") in moves
